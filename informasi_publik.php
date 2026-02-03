4ec56235180d39fdc60fc56c729fa96555f36897
Robby Hidayattullah <09021182126002@student.unsri.ac.id>
2026-02-03 10:53:23 +0700

    j

--- informasi_publik.php
+++ informasi_publik.php
@@ -1,47 +1,4 @@
-<!-- HEADER & NAVIGATION -->
-    <header id="menu-jk">
-        <div id="nav-head" class="header-nav">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                        <div class="logo-container">
-                            <img src="assets/images/imipas.png" alt="Logo Pemasyarakatan" class="company-logo">
-                            <img src="assets/images/pemasyarakatann.png" alt="Logo IMIPAS" class="company-logo">
-                            <span class="logo-text d-none d-lg-inline">LEMBAGA<br>PEMASYARAKATAN<br>KELAS IIA LUBUK LINGGAU</span>
-                        </div>
-                    </div>
-
-                    <div class="col-6 d-md-none text-right">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
-                            <i class="fas fa-bars small-menu"></i>
-                        </button>
-                    </div>
-
-                    <div id="menu" class="col-lg-9 col-md-8 col-12 collapse navbar-collapse nav-item">
-                        <ul>
-                            <li><a href="#home">Beranda</a></li>
-                            <li class="has-dropdown">
-                                <a href="#profil">Profil</a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#visi_misi">Visi & Misi</a></li>
-                                    <li><a href="#struktur">Struktur Organisasi</a></li>
-                                    <li><a href="#about_us">Tentang Kami</a></li>
-                                    <li><a href="#contact_us">Kontak</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#berita">Berita</a></li>
-                            <li><a href="#services">Layanan</a></li>
-                            <li><a href="#gallery">Galeri</a></li>
-                            <li><a href="informasi_publik.php">Informasi Publik</a></li>
-                            <li><a href="hms/admin">Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- INFORMASI PUBLIK SECTION -->
+ <!-- INFORMASI PUBLIK SECTION -->
     <section id="informasi_publik" class="informasi-publik-section">
         <div class="container">
             <div class="section-title text-center">
